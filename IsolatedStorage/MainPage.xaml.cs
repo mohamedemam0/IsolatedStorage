f66ccc03ec02d9def2463626ac5bb9071cc41c4d
Mohamed Emam <mohamedemam0@outlook.com>
2015-09-23 02:27:27 +0200

    a

---
+++ IsolatedStorage/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace IsolatedStorage
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+
+            if (!localSettings.Values.ContainsKey("Key1"))
+            {
+                localSettings.Values.Add("key1", "value1");
+            }
+
+            if (localSettings.Values.ContainsKey("key1"))
+            {
+                xx.Text = (String)localSettings.Values["key1"];
+            }
+        }
+    }
+}
